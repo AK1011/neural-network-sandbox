8752e11f966e54fe33b8a0781dbc787196fb90b8
Austin Kidd <austin.kidd@workday.com>
2017-04-19 11:51:54 -0700

    Added reasoning in digit file so I remember later why I'm doing that

--- src/digit_loader.py
+++ src/digit_loader.py
@@ -11,3 +11,6 @@ def load_digit_data():
    validation_data = zip(training_validation_test_inputs, validation_test_results)
    test_data = zip(training_validation_test_inputs, validation_test_results)
    return (training_data, validation_data, test_data)
+
+
+#The idea is to see if with only a few input possibilities to learn from, it can learn more precisely for just those. If it can learn well enough for that, we can observe each of the intermediate neurons to hopefully see how they actually interact to form an opinion on the input. Basically, with very few input possibilities, we can pick a delta that should have a large enough impact at the input level and observe exactly how that affects the next level in practice on a learned system.
